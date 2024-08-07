fb4d7071b831da3a7a84c60a96ca34a322c577e2
Gabriela Mameri <108195738+gabimati@users.noreply.github.com>
2024-08-06 21:11:59 -0300

    add Files

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function calcularValor(valor1, valor2) {
+    return valor1 * valor2;
+}
+console.log(calcularValor(20, 30));
+function saudacao(nome) {
+    return 'Olá' + nome;
+}
+console.log(saudacao('professor'));
---
+++ src/exercicio.ts
@@ -0,0 +1,12 @@
+function calcularValor (valor1:number, valor2:number) {
+    return valor1 *valor2
+}
+
+console.log(calcularValor(20, 30));
+
+
+function saudacao (nome: string) {
+    return 'Olá' + nome
+}
+
+console.log(saudacao('professor'));
